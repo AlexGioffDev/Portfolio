14b5054b26f6533faeed671c7f5f19228a0b756b
AlexGioffDev <alex.gioffre_dev@outlook.it>
2025-05-07 16:10:15 +0200

    Javascript about done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,60 @@
 let links = document.getElementsByClassName("link");
 let contents = document.getElementsByClassName("content-group");
+const langButtons = document.querySelectorAll(".buttons-container .btn");
+const aboutTexts = document.querySelectorAll(".about-texts .content-text");
+
+console.log({ aboutTexts });
 
 Array.from(links).forEach((link) => {
   link.addEventListener("click", function (e) {
-    e.preventDefault(); // Prevent default anchor behavior
+    e.preventDefault();
 
     if (this.classList.contains("active")) return;
 
-    // Update active link
     Array.from(links).forEach((l) => l.classList.remove("active"));
     this.classList.add("active");
 
     const targetId = this.getAttribute("href").replace("#", "");
     const targetContent = document.getElementById(targetId);
 
-    // Fade out current content
     Array.from(contents).forEach((c) => {
       c.classList.remove("content-active");
       c.classList.add("content-out");
     });
 
-    // Wait for animation to finish, then show new content
     setTimeout(() => {
       Array.from(contents).forEach((c) => c.classList.remove("content-out"));
       if (targetContent) {
         targetContent.classList.add("content-active");
       }
-    }, 300); // Match this to your CSS transition time
+    }, 500);
+  });
+});
+
+langButtons.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (this.classList.contains("btn-active")) return;
+
+    langButtons.forEach((btn) => btn.classList.remove("btn-active"));
+
+    this.classList.add("btn-active");
+    let targetID = this.innerText.toLowerCase();
+    const targetContent = document.getElementById(targetID);
+
+    aboutTexts.forEach((about) => {
+      if (about.classList.contains("text-active")) {
+        about.classList.remove("text-active");
+        about.classList.add("text-out");
+      }
+    });
+
+    setTimeout(() => {
+      aboutTexts.forEach((about) => about.classList.remove("text-out"));
+      if (targetContent) {
+        targetContent.classList.add("text-active");
+      }
+    }, 500);
   });
 });
