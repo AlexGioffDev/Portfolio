dafa0ba26f24ba7d8ebe69263ad48eef9992a98f
AlexGioffDev <alex.gioffre_dev@outlook.it>
2025-06-19 18:14:29 +0200

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ const headerNav = document.getElementById("header-nav");
 const buttonMenu = document.getElementById("buttonMenu");
 const linkButtons = document.getElementsByClassName("link-button");
 const html = document.getElementsByTagName("html");
+const scrollButton = document.getElementById("scrollbutton");
+const projectSection = document.getElementById("projects");
+
+const spanDate = document.getElementById("date");
 
 buttonMenu.addEventListener("click", () => {
   buttonMenu.classList.toggle("active");
@@ -16,4 +20,10 @@ Array.from(linkButtons).forEach((button) => {
     headerNav.classList.remove("active");
     html[0].style.overflowY = "auto";
   });
+});
+
+spanDate.innerText = new Date().getFullYear();
+
+scrollButton.addEventListener("click", () => {
+  projectSection.scrollIntoView({ behavior: "smooth" });
 });
